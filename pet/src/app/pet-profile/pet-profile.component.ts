e52f6bff422b88ac8e5a3f922e460e1c620bb912
Ann <asasi@ucsc.edu>
2025-04-15 13:35:53 -0700

    databinded pet

--- pet/src/app/pet-profile/pet-profile.component.ts
+++ pet/src/app/pet-profile/pet-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-pet-profile',
@@ -8,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './pet-profile.component.scss'
 })
 export class PetProfileComponent {
-
+name = signal('Akira');
+age = signal(20);
+// petname = input<string>(); string input signal
+// age = input(0); number input signal
+// 
+moods: string[] = ["lazy", "hungry", "angry", "curious","hyper", "tired"];
+currentMood = signal('');
+getRandomMood():void{
+const index = Math.floor(Math.random()* this.moods.length);
+this.currentMood.set(this.moods[index]);
+}
 }
